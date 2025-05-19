9a377ab37a501d2f7c1bf33cf0c5aaab542aee56
Igor Marcondes <124473250+IgorMarcondess@users.noreply.github.com>
2025-05-19 00:05:21 -0300

    initial

---
+++ N.I.B/src/main/java/com/fiap/N/I/B/controller/ProfissionalController.java
@@ -0,0 +1,122 @@
+package com.fiap.N.I.B.controller;
+
+import com.fiap.N.I.B.Repositories.ProfissionalRepository;
+import com.fiap.N.I.B.model.Profissional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@Controller
+@RequestMapping("/profissional")
+@RequiredArgsConstructor
+public class ProfissionalController {
+
+    private final ProfissionalRepository profissionalRepository;
+
+    @GetMapping
+    public ModelAndView listarProfissionais() {
+        List<Profissional> profissionais = profissionalRepository.findAll();
+        return new ModelAndView("Profissional/lista", "profissionais", profissionais);
+    }
+
+    @GetMapping("/novo")
+    public ModelAndView novoProfissionalForm() {
+        Profissional profissionalVazio = new Profissional();
+        return new ModelAndView("Profissional/cadastrar-profissional", "profissional", profissionalVazio);
+    }
+
+    @PostMapping("/novo")
+    public ModelAndView novoProfissional(@ModelAttribute Profissional profissionalParam) {
+        Optional<Profissional> profissionalExistente = profissionalRepository.findById(profissionalParam.getRegistroProfissional());
+
+        if (profissionalExistente.isPresent()) {
+            return new ModelAndView("Profissional/cadastrar-profissional", "erro", "Registro já cadastrado.");
+        }
+
+        // Validações dos campos
+        if (profissionalParam.getNomeProfissional().length() > 20) {
+            return new ModelAndView("Profissional/cadastrar-profissional", "erro", "O nome do profissional deve ter no máximo 20 caracteres.");
+        }
+
+        if (profissionalParam.getSobrenomeProfissional().length() > 30) {
+            return new ModelAndView("Profissional/cadastrar-profissional", "erro", "O sobrenome do profissional deve ter no máximo 30 caracteres.");
+        }
+
+        if (!profissionalParam.getTelefoneProfissional().matches("\\d{11}")) {
+            return new ModelAndView("Profissional/cadastrar-profissional", "erro", "O telefone deve conter exatamente 11 dígitos numéricos.");
+        }
+
+        Profissional novoProfissional = Profissional.builder()
+                .registroProfissional(profissionalParam.getRegistroProfissional())
+                .nomeProfissional(profissionalParam.getNomeProfissional())
+                .sobrenomeProfissional(profissionalParam.getSobrenomeProfissional())
+                .telefoneProfissional(profissionalParam.getTelefoneProfissional())
+                .emailProfissional(profissionalParam.getEmailProfissional())
+                .tipoProfissional(profissionalParam.getTipoProfissional())
+                .dataInscricaoProfissional(profissionalParam.getDataInscricaoProfissional())
+                .endereco(profissionalParam.getEndereco())
+                .consultas(profissionalParam.getConsultas())
+                .build();
+        profissionalRepository.save(novoProfissional);
+
+        return new ModelAndView("redirect:/profissional", "sucesso", "Profissional cadastrado com sucesso!");
+    }
+
+    @GetMapping("/{registroProfissional}")
+    public ModelAndView listarProfissional(@PathVariable String registroProfissional) {
+        Optional<Profissional> profissionalOptional = profissionalRepository.findById(registroProfissional);
+        if (profissionalOptional.isPresent()) {
+            return new ModelAndView("Profissional/listar-profissional", "profissional", profissionalOptional.get());
+        }
+        return new ModelAndView("redirect:/profissional", "erro", "Profissional não encontrado.");
+    }
+
+    @GetMapping("/editar/{registroProfissional}")
+    public ModelAndView editarProfissionalForm(@PathVariable String registroProfissional) {
+        Optional<Profissional> profissionalOptional = profissionalRepository.findById(registroProfissional);
+        if (profissionalOptional.isPresent()) {
+            return new ModelAndView("Profissional/editar-profissional", "profissional", profissionalOptional.get());
+        }
+        return new ModelAndView("redirect:/profissional", "erro", "Profissional não encontrado.");
+    }
+
+    @PostMapping("/editar")
+    public ModelAndView atualizarProfissional(@ModelAttribute Profissional profissionalParam) {
+        Optional<Profissional> profissionalOptional = profissionalRepository.findById(profissionalParam.getRegistroProfissional());
+
+        if (profissionalOptional.isPresent()) {
+            Profissional profissionalAtualizado = Profissional.builder()
+                    .registroProfissional(profissionalParam.getRegistroProfissional())
+                    .nomeProfissional(profissionalParam.getNomeProfissional())
+                    .sobrenomeProfissional(profissionalParam.getSobrenomeProfissional())
+                    .telefoneProfissional(profissionalParam.getTelefoneProfissional())
+                    .emailProfissional(profissionalParam.getEmailProfissional())
+                    .tipoProfissional(profissionalParam.getTipoProfissional())
+                    .dataInscricaoProfissional(profissionalParam.getDataInscricaoProfissional())
+                    .endereco(profissionalParam.getEndereco())
+                    .consultas(profissionalParam.getConsultas())
+                    .build();
+
+            profissionalRepository.save(profissionalAtualizado);
+            return new ModelAndView("redirect:/profissional", "sucesso", "Profissional atualizado com sucesso!");
+        }
+
+        return new ModelAndView("Profissional/editar-profissional", "erro", "Erro ao atualizar o profissional.");
+    }
+
+    @GetMapping("/deletar/{registro}")
+    public ModelAndView deletarProfissional(@PathVariable String registro) {
+        Optional<Profissional> profissionalOptional = profissionalRepository.findById(registro);
+        if (profissionalOptional.isPresent()) {
+            profissionalRepository.deleteById(registro);
+            return new ModelAndView("redirect:/profissional", "sucesso", "Profissional deletado com sucesso!");
+        }
+        return new ModelAndView("redirect:/profissional", "erro", "Profissional não encontrado para exclusão.");
+    }
+}
---
+++ N.I.B/src/main/java/com/fiap/N/I/B/model/Profissional.java
@@ -0,0 +1,65 @@
+package com.fiap.N.I.B.model;
+
+import com.fiap.N.I.B.ignore.Endereco;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Profissional extends RepresentationModel<Profissional> {
+
+    @NotNull
+    @Size(max = 20, message = "Nome deve ter no máximo 20 caracteres")
+    private String nomeProfissional;
+
+    @NotNull
+    @Size(max = 30, message = "Sobrenome deve ter no máximo 30 caracteres")
+    private String sobrenomeProfissional;
+
+    @NotNull
+    @Pattern(regexp = "\\d{11}", message = "Telefone deve conter 11 dígitos numéricos")
+    private String telefoneProfissional;
+
+    @NotNull
+    @Size(max = 20, message = "Tipo de profissional deve ter no máximo 20 caracteres")
+    private String tipoProfissional;
+
+    @NotNull
+    private Date dataInscricaoProfissional;
+
+    @Id
+    @NotNull
+    @Size(max = 15, message = "Registro deve ter no máximo 15 caracteres")
+    private String registroProfissional;
+
+    @NotNull
+    @Email(message = "Informe um e-mail válido")
+    private String emailProfissional;
+
+    @OneToMany(mappedBy = "profissional", cascade = CascadeType.ALL)
+    private List<Consulta> consultas = new ArrayList<>();
+
+    @OneToOne
+    private Endereco endereco;
+
+    public void adicionarConsulta(Consulta consulta) {
+        consulta.setProfissional(this);
+        this.consultas.add(consulta);
+    }
+
+}
